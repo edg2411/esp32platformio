cf905d95e0d02f7c997a3ae93901049e1b12e7a6
Ezequiel Di Giulio <eze.digiulio@gmail.com>
2023-03-20 16:54:36 -0300

    project added

---
+++ macesp32/src/main.cpp
@@ -0,0 +1,29 @@
+// GET MAC ADDRESS
+
+#ifdef ESP32
+  #include <WiFi.h>
+#else
+  #include <ESP8266WiFi.h>
+#endif
+
+void setup(){
+  Serial.begin(115200);
+  Serial.println();
+  Serial.print("ESP Board MAC Address:  ");
+  Serial.println(WiFi.macAddress());
+}
+ 
+void loop(){
+
+}
+
+/*
+
+MAC
+94:B5:55:8E:39:50
+
+
+MAC ID419 HIDROTOR
+30:C6:F7:F3:48:20
+
+*/
